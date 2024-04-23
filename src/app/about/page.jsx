6ec899b9a0f4e5b62d68f9ffc9d3c3120d177a72
yohanse <mehabawyohanse793@gmail.com>
2024-04-23 11:00:11 +0300

    finishe skill portion

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,6 +2,7 @@
 import { motion } from "framer-motion";
 
 const AboutPage = () => {
+  const skills = ["Python", "JavaScript", "TypeScript", "React.js", "Next.js", "Talwind CSS", "MongoDB", "PostgresSQL", "MySQL", "Django", "Dotnet", "Express.js"];
   return (
     <motion.div
       className="h-full"
@@ -68,7 +69,39 @@ const AboutPage = () => {
             </motion.svg>
           </div>
           {/* SKILLS CONTAINER */}
-          <div>SKILLS</div>
+          <div className="flex flex-col gap-12 justify-center">
+            {/* SKILL TITLE */}
+            <h1 className="font-bold text-2xl">SKILLS</h1>
+            {/* SKILL LIST */}
+            <div className="flex gap-4 flex-wrap">
+              {skills.map(skill => <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                {skill}
+              </div>)}
+            </div>
+            {/* SCROLL SVG */}
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: "10px" }}
+              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
+            >
+              <path
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+            </motion.svg>
+          </div>
           {/* EXPERIENCE CONTAINER */}
           <div>EXPERIENCE</div>
         </div>
