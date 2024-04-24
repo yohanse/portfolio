e3896f303c22529da623d187ee60db27f309666e
yohanse <mehabawyohanse793@gmail.com>
2024-04-24 17:39:27 +0300

    done with the form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,12 @@
 "use client";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const ContactPage = () => {
   const text = "Say Hello";
+  const [sucess, setSucess] = useState(false);
+  const [error, setError] = useState(false);
+
   return (
     <motion.div
       className="h-full"
@@ -12,7 +16,7 @@ const ContactPage = () => {
     >
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* TEXT CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 flex justify-center items-center text-6xl">
+        <div className="h-1/2 lg:h-full lg:w-2/5 flex justify-center items-center text-6xl">
           <div>
             {text.split("").map((letter, index) => (
               <motion.span
@@ -32,13 +36,15 @@ const ContactPage = () => {
           </div>
         </div>
         {/* FORM CONTAINER */}
-        <form action="" className="h-1/2 lg:h-full lg:w-1/2">
+        <form action="" className="h-1/2 lg:h-full lg:w-3/5 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24">
           <span>Dear Yohanse</span>
-          <textarea rows="6"></textarea>
+          <textarea rows="6" className="bg-transparent border-b-2 border-b-black outline-none resize-none"></textarea>
           <span>My mail address is:</span>
-          <input type="text" />
+          <input type="text" className="bg-transparent border-b-2 border-b-black outline-none"/>
           <span>Regards</span>
-          <button></button>
+          <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">Send</button>
+          {sucess && <span className="text-green-600 font-semibold">Your message has been sent successfully!</span>}
+          {error && <span className="text-red-600 font-semibold">Something went wrong!</span>}
         </form>
       </div>
     </motion.div>
