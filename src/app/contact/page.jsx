deae21e1e6daafe436554ba382cd22d79722e5fa
yohanse <mehabawyohanse793@gmail.com>
2024-04-24 11:50:51 +0300

    finshe text container of contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,6 +2,7 @@
 import { motion } from "framer-motion";
 
 const ContactPage = () => {
+  const text = "Say Hello";
   return (
     <motion.div
       className="h-full"
@@ -9,9 +10,39 @@ const ContactPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      Contact Page
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex justify-center items-center text-6xl">
+          <div>
+            {text.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            😊
+          </div>
+        </div>
+        {/* FORM CONTAINER */}
+        <form action="" className="h-1/2 lg:h-full lg:w-1/2">
+          <span>Dear Yohanse</span>
+          <textarea rows="6"></textarea>
+          <span>My mail address is:</span>
+          <input type="text" />
+          <span>Regards</span>
+          <button></button>
+        </form>
+      </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default ContactPage
+export default ContactPage;
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -11,21 +11,69 @@ const PortfolioPage = () => {
       transition={{ duration: 1 }}
     >
       <div className="h-full overflow-scroll grid gap-5 grid-cols-1 sm:grid-cols-1  md:grid-cols-2  lg:grid-cols-3  xl:grid-cols-3  px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-        <ProjectsCard imgUrl={"/hero.png"} title={"My Project"} description={"Done with great attitude"}></ProjectsCard>
-        <ProjectsCard imgUrl={"/hero.png"} title={"My Project"} description={"Done with great attitude"}></ProjectsCard>
-        <ProjectsCard imgUrl={"/hero.png"} title={"My Project"} description={"Done with great attitude"}></ProjectsCard>
-        <ProjectsCard imgUrl={"/hero.png"} title={"My Project"} description={"Done with great attitude"}></ProjectsCard>
-        <ProjectsCard imgUrl={"/hero.png"} title={"My Project"} description={"Done with great attitude"}></ProjectsCard>
-        <ProjectsCard imgUrl={"/hero.png"} title={"My Project"} description={"Done with great attitude"}></ProjectsCard>
-        <ProjectsCard imgUrl={"/hero.png"} title={"My Project"} description={"Done with great attitude"}></ProjectsCard>
-        <ProjectsCard imgUrl={"/hero.png"} title={"My Project"} description={"Done with great attitude"}></ProjectsCard>
-        <ProjectsCard imgUrl={"/hero.png"} title={"My Project"} description={"Done with great attitude"}></ProjectsCard>
-        <ProjectsCard imgUrl={"/hero.png"} title={"My Project"} description={"Done with great attitude"}></ProjectsCard>
-        <ProjectsCard imgUrl={"/hero.png"} title={"My Project"} description={"Done with great attitude"}></ProjectsCard>
-        <ProjectsCard imgUrl={"/hero.png"} title={"My Project"} description={"Done with great attitude"}></ProjectsCard>
+        <ProjectsCard
+          imgUrl={"/hero.png"}
+          title={"My Project"}
+          description={"Done with great attitude"}
+        ></ProjectsCard>
+        <ProjectsCard
+          imgUrl={"/hero.png"}
+          title={"My Project"}
+          description={"Done with great attitude"}
+        ></ProjectsCard>
+        <ProjectsCard
+          imgUrl={"/hero.png"}
+          title={"My Project"}
+          description={"Done with great attitude"}
+        ></ProjectsCard>
+        <ProjectsCard
+          imgUrl={"/hero.png"}
+          title={"My Project"}
+          description={"Done with great attitude"}
+        ></ProjectsCard>
+        <ProjectsCard
+          imgUrl={"/hero.png"}
+          title={"My Project"}
+          description={"Done with great attitude"}
+        ></ProjectsCard>
+        <ProjectsCard
+          imgUrl={"/hero.png"}
+          title={"My Project"}
+          description={"Done with great attitude"}
+        ></ProjectsCard>
+        <ProjectsCard
+          imgUrl={"/hero.png"}
+          title={"My Project"}
+          description={"Done with great attitude"}
+        ></ProjectsCard>
+        <ProjectsCard
+          imgUrl={"/hero.png"}
+          title={"My Project"}
+          description={"Done with great attitude"}
+        ></ProjectsCard>
+        <ProjectsCard
+          imgUrl={"/hero.png"}
+          title={"My Project"}
+          description={"Done with great attitude"}
+        ></ProjectsCard>
+        <ProjectsCard
+          imgUrl={"/hero.png"}
+          title={"My Project"}
+          description={"Done with great attitude"}
+        ></ProjectsCard>
+        <ProjectsCard
+          imgUrl={"/hero.png"}
+          title={"My Project"}
+          description={"Done with great attitude"}
+        ></ProjectsCard>
+        <ProjectsCard
+          imgUrl={"/hero.png"}
+          title={"My Project"}
+          description={"Done with great attitude"}
+        ></ProjectsCard>
       </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default PortfolioPage
+export default PortfolioPage;
